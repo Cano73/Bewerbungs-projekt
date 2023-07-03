9264828c26a024b7f9b8c0b74a3f4d35eb4868b7
Cok özel <kqsqp.ck@gmail.com>
2023-07-03 12:12:29 +0200

    Initial Commit

---
+++ Frisoer/FrisoerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrisoerApp.swift
+//  Frisoer
+//
+//  Created by Can Kasap on 03.07.23.
+//
+
+import SwiftUI
+
+@main
+struct FrisoerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
